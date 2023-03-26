bec8fd8e6d843e04f83d72b29a000040d7d6a9eb
nolastname <43694360+toor404@users.noreply.github.com>
2023-03-26 17:13:07 +0700

    Update server.js
    change port 3000 to 80

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const mimeTypes = {
 
 // Hostname and Port
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 80;
 
 // Create Server
 const server = http.createServer((req, res) => {
